0822ec2fc0c22ce45cc072dd2d58b2e7710c294c
Itscoldhere <hougentan@gmail.com>
2026-02-20 14:32:21 +0800

    change return value for getNfts

--- src/transactions/getNfts.ts
+++ src/transactions/getNfts.ts
@@ -10,20 +10,21 @@ export const getNfts = async (wallet: Wallet) => {
     const targetURI = "53656174206E756D6265723A20313741";
 
     // Get all NFTs
-    const nfts = await client.request({
-        // method: "account_nfts",
+    const allNfts = await client.request({
         account: wallet.classicAddress,
         command: "account_nfts",
     });
     
     // Filter NFTs by URI
-    const ticket = nfts.result.account_nfts.filter(nft => {
+    const nft = allNfts.result.account_nfts.filter(nft => {
         if (!nft.URI) return false;
 
         // Decode URI from hex to UTF-8
         return nft.URI == targetURI;
     });
 
-    results = '\n=========\nTicket:\n' + JSON.stringify(ticket, null, 2) + '\n=========';
+    results = '\n=========\nTicket:\n' + JSON.stringify(nft, null, 2) + '\n=========';
     // console.log(`Result: ${results}`);
+
+    return nft;
 }
